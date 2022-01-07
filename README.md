278e5e38f95cc880dd22672b7ffd55e6b99f04b7
Tobias Balle-Petersen <tbp@one.com>
2022-01-07 17:46:34 +0100

    Added support for simple math problems

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# math-problem-generator
-A python package for generating simple math problems
+# Python package math_problem_generator
+A python package for generating simple math problems. The package is only for
+generating the problems. There is no support for presentation or user interaction.
+
+# Examples
+How to use the package.
+
+```python
+# Import the package
+from math_problem_generator import generator
+
+# Generate 2 simple addition problems with
+# 4 numbers between 1 and 10 to be added
+p = generator.simple_problems(
+        "add",no_of_problems=2,min_number=1,max_number=10,numbers=4
+    )
+
+# Print the math problems
+print(p)
+```
+
+The code above should print something like this:
+```
+[
+    {'type': 'simple', 'operator': 'add', 'numbers': [5, 2, 3, 6], 'result': 16, 'users_answer': None},
+    {'type': 'simple', 'operator': 'add', 'numbers': [6, 10, 10, 5], 'result': 31, 'users_answer': None}]
+]
+```
+
+# Development
+
+Setup virtual environment
+1. `python3 -m venv .venv`
+2. `source .venv/bin/activate`
+3. `pip3 install -r requirements-dev.txt`
+
+Leave virtual environment
+1. `deactivate`
---
+++ src/math_problem_generator/generator.py
@@ -0,0 +1,95 @@
+"""
+Math problem generator. Needs python 3.8 or higher.
+"""
+import operator
+import random
+
+from typing import List, TypedDict, Union
+
+
+class SimpleMathProblem(TypedDict):
+    type: str
+    operator: str
+    numbers: List[Union[int, float]]
+    result: Union[None, int, float]
+    users_answer: Union[None, int, float]
+
+
+def get_result(operator_name: str, numbers: List[int]) -> Union[None, int, float]:
+    """
+    Calculate the result for a list of numbers and an operator
+    """
+
+    # Define the operator function to use
+    if operator_name == "add":
+        o = operator.add
+    elif operator_name == "sub":
+        o = operator.sub
+    elif operator_name == "mul":
+        o = operator.mul
+    elif operator_name == "div":
+        o = operator.truediv
+    else:
+        raise ValueError(f"Invalid operator: '{operator_name}'")
+
+    # Calculate result
+    r = numbers[0]
+    for n in numbers[1:]:
+        try:
+            r = o(r, n)
+        except ZeroDivisionError:
+            return None
+
+    return r
+
+
+def simple_problems(
+    op: str,
+    no_of_problems: int = 25,
+    min_number: int = 1,
+    max_number: int = 100,
+    numbers: int = 2,
+    seed: Union[None, str] = None,
+    decimals: int = 4,
+    sort_numbers: bool = False,
+) -> List[SimpleMathProblem]:
+    """
+    Generate simple math problems like 1+2=3
+    """
+
+    # Can't generate problems with less than 2 numbers
+    if numbers < 2:
+        raise ValueError("Argument 'numbers' less that 2")
+
+    # Supply sees for predictable problems
+    if seed:
+        random.seed(seed)
+
+    problem_list = []
+
+    # Generate the problems
+    for i in range(no_of_problems):
+        # Empty problem dict
+        p: SimpleMathProblem = {
+            "type": "simple",
+            "operator": op,
+            "numbers": [],
+            "result": None,
+            "users_answer": None,
+        }
+
+        # Generate numbers for problem
+        for i in range(numbers):
+            p["numbers"].append(random.randint(min_number, max_number))
+
+        # Sort highest numbers first
+        if sort_numbers:
+            p["numbers"].sort(reverse=True)
+
+        # Add the result to the problem
+        p["result"] = get_result(op, p["numbers"])
+
+        # Add problem to list of problems
+        problem_list.append(p)
+
+    return problem_list
